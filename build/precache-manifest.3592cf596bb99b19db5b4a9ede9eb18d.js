5c511b93c3056830d7fc9ea1c3f9824994c80a16
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2020-01-22 21:24:42 +0200

    build

---
+++ build/precache-manifest.3592cf596bb99b19db5b4a9ede9eb18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb158d6124fe77e1c27acfb0f06871f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6743dd12ce77d60a479c",
+    "url": "/static/css/main.77ac519b.chunk.css"
+  },
+  {
+    "revision": "02ac46f5a59ec19315a6",
+    "url": "/static/js/2.3f601b78.chunk.js"
+  },
+  {
+    "revision": "618ad663a35d9fe4de17779cce26cbd7",
+    "url": "/static/js/2.3f601b78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6743dd12ce77d60a479c",
+    "url": "/static/js/main.fb57d59a.chunk.js"
+  },
+  {
+    "revision": "9c4e6907c921bf7670e8",
+    "url": "/static/js/runtime-main.b3073c9c.js"
+  },
+  {
+    "revision": "386ac8afd5ae27261338606720f3834c",
+    "url": "/static/media/tanvi.386ac8af.jpg"
+  }
+]);
